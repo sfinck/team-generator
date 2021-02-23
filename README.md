e5f6d88489a9260127f005eca4006d80f75b57c5
Sarah Finck <sarahfinck95@gmail.com>
2021-02-22 21:36:45 -0600

    added templates for different employees, updated read me

--- README.md
+++ README.md
@@ -1,15 +1,49 @@
 # team-generator
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
-## User Story
+## Description 
+The Team Generator applicaiton is designed for the user to build a team of developers. They have the option of choosing a manager, engineers and interns. When the user adds a manager option they are required to give the name, id, email and office number. When the user adds an engineer, they are required to give the name, id, email and git hub username. When the user adds an intern, they are required to give the name, id, email and school name. The user is allowed to add as many engineers and interns as they would like. When they are completed with their team they have an option to choose 'All Done!', which will terminate the application. Once the data is collected, it is generated into an HTML file where it displays each team member in a separate card with their inputted information. In addition to adding the text to the html, it will display an icon that will represent what role the team member has. 
 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+## Installation 
+1. [JavaScript](https://www.javascript.com/) 
+2. [Node](https://nodejs.org/en/)
+3. [Inquirer package](https://www.npmjs.com/package/inquirer)
+4. [fs](https://www.npmjs.com/package/fs)
+5. [Path](https://www.npmjs.com/package/path)
+6. [Screencastify](https://www.screencastify.com/)
+7. [Font Awesome](https://fontawesome.com/)
+
+## Usage 
+The user will run 'node index.js' and they will be prompted with the following questions: 
+```md
+![alt text](assets/start-menu.png)
+```
+Next, the user will choose which team member they want to add:
+```md
+![alt text](assets/user-option.png)
+```
+If the user tries to enter a previous ID then "ID already exists" message will appear:
+```md
+![alt text](assets/id-already-exists.png)
+```
+Once the user is completed with their team, the application will end and the index.html will open and display their team: 
+```md
+![alt text](assets/all-done.png)
+```
+
+### User Story
 ```md
 AS A manager
 I WANT to generate a webpage that displays my team's basic info
 SO THAT I have quick access to their emails and GitHub profiles
 ```
 
-## Acceptance Criteria
-
+### Acceptance Criteria
 ```md
 GIVEN a command-line application that accepts user input
 WHEN I am prompted for my team members and their information
@@ -29,3 +63,5 @@ THEN I am prompted to enter the intern’s name, ID, email, and school, and I am
 WHEN I decide to finish building my team
 THEN I exit the application, and the HTML is generated
 ```
+## License 
+[MIT](https://opensource.org/licenses/MIT)
--- index.js
+++ index.js
@@ -1,34 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
-
-//start menu
-function startMenu () {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is your employee's name?",
-            name: 'employeeName',
-        },
-        {
-            type: 'input',
-            message: "What is your employee's email?",
-            name: 'employeeEmail',
-        },
-        {
-            type: 'input',
-            message: "What is your employee's ID?",
-            name: 'employeeId',
-        },
-    ]).then(results => {
-        const employee = new Employee (results.employeeName, results.employeeId, results.employeeEmail)
-    })
-}
-
-//if else statement 
+let idArray = [];
+let teamArray = [];
 
 //if user choose manager option 
 function managerOption () {
@@ -47,6 +25,12 @@ function managerOption () {
             type: 'input',
             message: "What is your manager's ID?",
             name: 'managerId',
+            validate: function(input){
+                if (idArray.indexOf(input)!==-1){
+                    return 'ID Already exists'
+                }
+                return true
+            }
         },
         {
             type: 'input',
@@ -55,10 +39,11 @@ function managerOption () {
         },
     ]).then(results => {
         const manager = new Manager (results.managerName, results.managerId, results.managerEmail, results.officeNumber)
+        idArray.push(results.managerId)
+        teamArray.push(manager)
+        userChoice();
     })
 }
-
-//if else statement 
 
 //if user chooses engineer option
 function engineerOption () {
@@ -82,12 +67,20 @@ function engineerOption () {
             type: 'input',
             message: "What is your engineers ID?",
             name: 'engineerId',
+            validate: function(input){
+                if (idArray.indexOf(input)!==-1){
+                    return 'ID Already exists'
+                }
+                return true
+            }
         },
     ]).then(results => {
         const engineer = new Engineer (results.engineerName, results.engineerId, results.githubUsername, results.engineerEmail)
+        idArray.push(results.engineerId)
+        teamArray.push(results.engineer)
+        userChoice();
     })
 }
-//if else statment 
 
 //if user chooses intern option
 function internOption () {
@@ -106,6 +99,12 @@ function internOption () {
             type: 'input',
             message: "What is your intern's ID?",
             name: 'internId',
+            validate: function(input){
+                if (idArray.indexOf(input)!==-1){
+                    return 'ID Already exists'
+                }
+                return true
+            }
         },
         {
             type: 'input',
@@ -114,9 +113,44 @@ function internOption () {
         },
     ]).then(results => {
         const intern = new Intern (results.internName, results.internId, results.internEmail, results.schoolName)
+        idArray.push(results.internId)
+        teamArray.push(results.intern)
+        userChoice();
     })
 }
+
 //create function for engineer, intern
-    //.then if else statment - call function to ask intern questions or engineer questions
-//create a function to go back to menu 
-//create a function to add member
+//.then if else statment - call function to ask intern questions or engineer questions
+function userChoice () {
+    inquirer.prompt([
+        {  
+        type: 'list',
+        message: "Which type of team member would you like to add?",
+        name: 'userChoice',
+        choices: ['Engineer', 'Intern', 'All done!'],
+        }
+    ]).then(results => {
+        if (results.userChoice === 'Engineer'){
+            engineerOption();
+        }
+        else if (results.userChoice === 'Intern'){
+            internOption();
+        }
+        else {
+            console.log("done");
+        }
+    })
+}
+
+function teamHTML() {
+    if (!fs.existsSync(path.resolve(__dirname,'output'))){
+        fs.mkdirSync(path.resolve(__dirname,'output'))
+    }
+    
+//write code that makes file 
+//how to render onto html
+}
+
+managerOption();
+
+
